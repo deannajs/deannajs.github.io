d62a71c13affcee38b6aeb2e57d4b7919df50e0d
Deanna <deanna@Deannas-MacBook-Air.local>
2020-09-03 11:41:10 -0400

    asdf

---
+++ send_email.php
@@ -0,0 +1,14 @@
+<?php
+$to      = 'deannajs@umich.edu';
+$subject = 'from user on deannajs.github.io';
+$message = 'hello';
+
+echo $_POST['name'];
+
+
+// $headers = 'From: webmaster@example.com' . "\r\n" .
+//     'Reply-To: webmaster@example.com' . "\r\n" .
+//     'X-Mailer: PHP/' . phpversion();
+
+// mail($to, $subject, $message, $headers);
+?> 
