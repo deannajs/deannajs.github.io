be6b0946c408a713fc3f4532aac41bbcbd4d409d
Deanna <deanna@Deannas-MacBook-Air.local>
2020-09-03 17:38:26 -0400

    asdf

--- deanna.js
+++ deanna.js
@@ -2,7 +2,6 @@ window.onload = function(){
   document.getElementById("scroll_button").onclick = function() {scrollToAboutMe()};
 
   function scrollToAboutMe() {
-    console.log("hellooooo");
     document.getElementById("about me").scrollIntoView();
   }
 }
