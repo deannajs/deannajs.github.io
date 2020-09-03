4552d339f488f91378edb0deb53082371b5cde72
Deanna <deanna@Deannas-MacBook-Air.local>
2020-09-03 15:35:17 -0400

    hello?

--- deanna.js
+++ deanna.js
@@ -1,13 +1,5 @@
-function openTab(event, tabname) {
-  // var i, tabcontent, tablinks;
-  var tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].innerText = "";
-  }
-  // tablinks = document.getElementsByClassName("tablinks");
-  // for (i = 0; i < tablinks.length; i++) {
-  //   tablinks[i].className = tablinks[i].className.replace(" active", "");
-  // }
-  // document.getElementById(tabname).style.display = "block";
-  // evt.currentTarget.className += " active";
+document.getElementById("button").onclick = function() {scrollToAboutMe()};
+
+function scrollToAboutMe() {
+  document.getElementById("about me").scrollIntoView();
 }
