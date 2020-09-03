7042d1c9a53d124813e57c541910d5dfa15e9806
Deanna <deanna@Deannas-MacBook-Air.local>
2020-09-03 12:32:18 -0400

    hello?

--- send_email.php
+++ send_email.php
@@ -2,32 +2,5 @@
   $name = $_POST['name'];
   $visitor_email = $_POST['email'];
   $message = $_POST['message'];
-
-
-  // echo "HELLO";
-
-// if(isset($_POST['submit'])){
-//     echo "YOU SUBMITTED A FORM GOOD JOB";
-// }
-// $to      = 'deannajs@umich.edu';
-// $subject = 'from user on deannajs.github.io';
-// $message = 'hello';
-
-// echo $to;
-// echo $subject;
-// echo $message;
-
-// var_dump($_POST);
-// echo $_POST["name"];
-// echo $_POST["email"];
-// echo $_POST["message"];
-
-
-// $headers = 'From: webmaster@example.com' . "\r\n" .
-//     'Reply-To: webmaster@example.com' . "\r\n" .
-//     'X-Mailer: PHP/' . phpversion();
-
-// mail($to, $subject, $message, $headers);
-
 ?>
 
