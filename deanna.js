3c31c21bb73ccf1a9665922f65f01362b660ec3b
Deanna <deanna@Deannas-MacBook-Air.local>
2020-09-05 13:57:28 -0400

    asdf

--- deanna.js
+++ deanna.js
@@ -14,29 +14,3 @@ function updateScrollProgress() {
   var scrolled = (winScroll / height) * 100;
   document.getElementById("myBar").style.width = scrolled + "%";
 }
-
-
-$(function() {
-$(document).ready(function() {
-    
-  /* Every time the window is scrolled ... */
-  $(window).scroll( function(){
-  
-      /* Check the location of each desired element */
-          
-      var bottom_of_object = $(".section_header").position().top + $(".section_header").outerHeight();
-      var bottom_of_window = $(window).scrollTop() + $(window).height();
-      
-      /* If the object is completely visible in the window, fade it in */
-      if( bottom_of_window > bottom_of_object ){
-        console.log('navbar should be visible');
-        $("#navbar").fadeIn();
-      } else {
-        console.log('navbar should NOT be visible');
-        $("#navbar").fadeOut();
-      }
-  
-  });
-  
-});
-});
