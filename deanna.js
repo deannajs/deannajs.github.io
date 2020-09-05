43bced281b4684cf99453c8c1e128e58a74673a3
Deanna <deanna@Deannas-MacBook-Air.local>
2020-09-04 23:57:02 -0400

    asdf

--- deanna.js
+++ deanna.js
@@ -5,3 +5,12 @@ window.onload = function(){
     document.getElementById("about me").scrollIntoView();
   }
 }
+
+window.onscroll = function() {updateScrollProgress()};
+
+function updateScrollProgress() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
