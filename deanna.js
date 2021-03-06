77cf0386393f2520ee2acdad43b4bbcb06043f5f
deannajs <deannajs@umich.edu>
2021-03-06 17:36:50 -0500

    background

--- deanna.js
+++ deanna.js
@@ -1,13 +1,15 @@
-document.getElementById("scroll_button").onclick = function() {scrollToAboutMe()};
+window.onload() = function() {
+  document.getElementById("scroll_button").onclick = function() {scrollToAboutMe()};
 
-function scrollToAboutMe() {
-  console.log("scrolling to about me");
-  var element = document.getElementById("about me");
-  var headerOffset = 13;
-  var elementPosition = element.offsetTop();
-  var offsetPosition = elementPosition - headerOffset;
+  function scrollToAboutMe() {
+    console.log("scrolling to about me");
+    var element = document.getElementById("about me");
+    var headerOffset = 13;
+    var elementPosition = element.offsetTop();
+    var offsetPosition = elementPosition - headerOffset;
 
-  window.scrollTo({top: offsetPosition, behavior: "smooth"})
+    window.scrollTo({top: offsetPosition, behavior: "smooth"})
+  }
 }
 
 window.onscroll = function() {updateScrollProgress()};
