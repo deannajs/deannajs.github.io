32425a78e42a8b96533e8f548aef93a0845ca7b6
deannajs <deannajs@umich.edu>
2021-03-19 02:48:51 -0400

    stuff

---
+++ deanna.js
@@ -0,0 +1,34 @@
+
+window.onload = function() {
+  document.getElementById("scroll_button").onclick = scrollToId('about');
+}
+
+window.onscroll = function() {
+  updateScrollProgress();
+  updateBackgroundColor();
+}
+
+function updateScrollProgress() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+function scrollToId(idName) {
+  var element = document.getElementById(idName);
+  var headerOffset = document.getElementById("navbar").offsetHeight;
+  var elementPosition = element.offsetTop;
+  var offsetPosition = elementPosition - headerOffset;
+
+  window.scrollTo({top: offsetPosition, behavior: "smooth"});
+}
+
+function updateBackgroundColor() {
+  var element = document.getElementById("navbar");
+  var bodyBackgroundColor = window.getComputedStyle(document.body, null).getPropertyValue('background');
+  console.log("background color type is ", typeof(bodyBackgroundColor));
+  console.log("background color is ", bodyBackgroundColor);
+  console.log("background color element 0 is ", bodyBackgroundColor[0]);
+  element.style.backgroundColor = bodyBackgroundColor;
+}
