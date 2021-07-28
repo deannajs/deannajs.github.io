7f92409b5a38b57bf21f119678daa64514643be3
deannajs <deannajs@umich.edu>
2021-07-28 01:47:42 -0400

    recomment

--- deanna.js
+++ deanna.js
@@ -1,7 +1,6 @@
-
-// window.onload = function() {
-//   document.getElementById("scroll_button").onclick = scrollToId('about me');
-// }
+window.onload = function() {
+  document.getElementById("scroll_button").onclick = scrollToId('about me');
+}
 
 window.onscroll = function() {
   updateScrollProgress();
