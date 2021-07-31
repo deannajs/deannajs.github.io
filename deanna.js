bd01d8ae5828c52b2f902b97ad555ac45619741e
deannajs <deannajs@umich.edu>
2021-07-31 02:04:05 -0400

    okurrrrr

--- deanna.js
+++ deanna.js
@@ -105,10 +105,9 @@ function typewrite(strings, curStringIdx, curStrPos) {
 
         // add one char at a time
         curStrPos++;
-
-        // loop the function
-        typewrite(strings, curStringIdx, curStrPos);
       }
+      // loop the function
+      typewrite(strings, curStringIdx, curStrPos);
     })
   }, 1000)
 }
