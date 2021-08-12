2246de3c96aae5a7270dc11eb892dc27417c4d95
deannajs <deannajs@umich.edu>
2021-08-12 00:55:58 -0400

    ah?

--- deanna.js
+++ deanna.js
@@ -15,6 +15,23 @@ window.onload = function() {
 window.onscroll = function() {
   updateScrollProgress();
   updateNavbarColor();
+  fadeIn();
+}
+
+function fadeIn() {
+  var profilepic = document.getElementById("profilepic");
+
+  var bottom_of_object = profilepic.position().top + profilepic.outerHeight();
+  var bottom_of_window = window.scrollTop() + window.height();
+
+  /* If the object is completely visible in the window, fade it it */
+  if (bottom_of_window > bottom_of_object) {
+
+      profilepic.animate({
+          'opacity': '1'
+      }, 500);
+
+  }
 }
 
 function updateScrollProgress() {
