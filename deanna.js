0575d1d2b907b44669707e7982eae45fbc81844e
deannajs <deannajs@umich.edu>
2021-08-12 23:53:00 -0400

    asdlfadjs;ughhhh

--- deanna.js
+++ deanna.js
@@ -243,7 +243,6 @@ function showSlides(slides, nextSlideIdxs) {
   // assign grid-column positions
   for (var i=0; i < nextSlideIdxs.length; i++) {
     slides[nextSlideIdxs[i]].style.gridColumn = i + 2; // starts at 1, and first column is left arrow
-    console.log('slide ', nextSlideIdxs[i], ' is in col ', i + 2);
   }
 }
 
